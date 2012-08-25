563456abe0fb875660212cf14246433d6a456d3e
Gabriel Bretschner <info@kanedo.net>
2012-08-25 17:59:33 +0200

    added comments and example to kanedo_protected_post_password

--- kanedo_protected_post_password.php
+++ kanedo_protected_post_password.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This function automatically protects certain posttypes with one password
+ * @param Object $post The Post to be protected
+ * @param string $password Password
+ * @param string $posttype The custom post type
+ * */
 function kanedo_protected_post_password($post, $password, $posttype) {
     if ($post['post_type'] != $posttype)
         return $post;
@@ -7,4 +13,8 @@ function kanedo_protected_post_password($post, $password, $posttype) {
     $post['post_status'] = 'publish';
     return $post;
 }
-add_filter('wp_insert_post_data', 'kanedo_protected_post_password');
+//A Simple implementation of the function
+function protect_post($post){
+	return kanedo_protected_post_password($post, 'aSimplePassword', 'myCustomPostType');
+}
+add_filter('wp_insert_post_data', 'protect_post');
